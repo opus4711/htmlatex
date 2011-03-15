952ec9f155fc5d4ab585e2bd2b2ee8b112bd8d21
opus4711 <b-bass@versanet.de>
2011-03-15 17:31:58 +0100

    Fixed translation

--- htmlatex_de.ts
+++ htmlatex_de.ts
@@ -5,27 +5,27 @@
 <context>
     <name>CConsole</name>
     <message>
-        <location filename="cconsole.cpp" line="42"/>
+        <location filename="cconsole.cpp" line="41"/>
         <source>Read source file(s) --&gt; success</source>
         <translation>Lesen der Quelldatei(en)  --&gt; erfolgreich</translation>
     </message>
     <message>
-        <location filename="cconsole.cpp" line="47"/>
+        <location filename="cconsole.cpp" line="46"/>
         <source>I/O error - the source file doesn&apos;t exit: </source>
         <translation>E/A Fehler - Die Quelldatei existiert nicht.</translation>
     </message>
     <message>
-        <location filename="cconsole.cpp" line="65"/>
+        <location filename="cconsole.cpp" line="64"/>
         <source>Perform conversion  --&gt; success</source>
         <translation>Konvertierung durchführen --&gt; erfolgreich</translation>
     </message>
     <message>
-        <location filename="cconsole.cpp" line="69"/>
+        <location filename="cconsole.cpp" line="68"/>
         <source>I/O error - can&apos;t write to file: </source>
         <translation>E/A Fehler - Kann die Datei nicht zum Schreiben öffnen:</translation>
     </message>
     <message>
-        <location filename="cconsole.cpp" line="75"/>
+        <location filename="cconsole.cpp" line="74"/>
         <source>Error - unexpected number of arguments.</source>
         <translation>Fehler - unerwartete Parameterzahl.</translation>
     </message>
@@ -34,64 +34,82 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="ui_mainwindow.h" line="93"/>
+        <location filename="ui_mainwindow.h" line="128"/>
         <source>HTMLatex</source>
         <translation>HTMLatex</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="42"/>
-        <location filename="ui_mainwindow.h" line="100"/>
+        <location filename="ui_mainwindow.h" line="136"/>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="57"/>
-        <location filename="mainwindow.cpp" line="15"/>
-        <location filename="ui_mainwindow.h" line="94"/>
+        <location filename="mainwindow.ui" line="54"/>
+        <location filename="ui_mainwindow.h" line="137"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hilfe</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="64"/>
+        <location filename="ui_mainwindow.h" line="138"/>
+        <source>toolBar</source>
+        <translation>Symbolleiste</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="83"/>
+        <location filename="mainwindow.cpp" line="13"/>
+        <location filename="ui_mainwindow.h" line="129"/>
         <source>&amp;Quit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="62"/>
-        <location filename="mainwindow.cpp" line="14"/>
-        <location filename="ui_mainwindow.h" line="95"/>
+        <location filename="mainwindow.ui" line="92"/>
+        <location filename="mainwindow.cpp" line="12"/>
+        <location filename="ui_mainwindow.h" line="130"/>
         <source>&amp;Convert</source>
         <translation>&amp;Konvertieren</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="67"/>
-        <location filename="mainwindow.cpp" line="13"/>
-        <location filename="ui_mainwindow.h" line="96"/>
+        <location filename="mainwindow.ui" line="101"/>
+        <location filename="mainwindow.cpp" line="11"/>
+        <location filename="ui_mainwindow.h" line="131"/>
         <source>&amp;Open</source>
         <translation>Ö&amp;ffnen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="72"/>
-        <location filename="ui_mainwindow.h" line="97"/>
+        <location filename="mainwindow.ui" line="106"/>
+        <location filename="ui_mainwindow.h" line="132"/>
         <source>Set &amp;Replacement Table</source>
         <translation>&amp;Ersetzungstabelle auswählen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="77"/>
-        <location filename="mainwindow.cpp" line="11"/>
-        <location filename="ui_mainwindow.h" line="98"/>
+        <location filename="mainwindow.ui" line="115"/>
+        <location filename="mainwindow.cpp" line="9"/>
+        <location filename="ui_mainwindow.h" line="133"/>
         <source>Set &amp;Input Definition</source>
         <translation>&amp;Eingabedefinition wählen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="82"/>
-        <location filename="mainwindow.cpp" line="12"/>
-        <location filename="ui_mainwindow.h" line="99"/>
+        <location filename="mainwindow.ui" line="124"/>
+        <location filename="mainwindow.cpp" line="10"/>
+        <location filename="ui_mainwindow.h" line="134"/>
         <source>Set O&amp;utput Definition</source>
         <translation>&amp;Ausgabedefinition wählen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="106"/>
+        <location filename="mainwindow.ui" line="133"/>
+        <location filename="ui_mainwindow.h" line="135"/>
+        <source>&amp;Info</source>
+        <translation>Üb&amp;er htmLaTeX</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="97"/>
         <source>Error Reading Document</source>
         <translation>Fehler beim Lesen der Datei</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="107"/>
+        <location filename="mainwindow.cpp" line="98"/>
         <source>The reading resulted in an empty document.
 Maybe an error occurred because of a wrong source file type:
 
@@ -102,13 +120,13 @@ Vielleicht ist der Typ der Quelldatei falsch:
 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="124"/>
-        <location filename="mainwindow.cpp" line="168"/>
+        <location filename="mainwindow.cpp" line="115"/>
+        <location filename="mainwindow.cpp" line="159"/>
         <source>I/O Error</source>
         <translation>E/A Fehler</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="125"/>
+        <location filename="mainwindow.cpp" line="116"/>
         <source>File doesn&apos;t exit:
 
 </source>
@@ -117,22 +135,22 @@ Vielleicht ist der Typ der Quelldatei falsch:
 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="156"/>
+        <location filename="mainwindow.cpp" line="147"/>
         <source>conversion successfully performed</source>
         <translation>Konvertierung erfolgreich</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="159"/>
+        <location filename="mainwindow.cpp" line="150"/>
         <source>Information</source>
         <translation>Hinweis</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="160"/>
+        <location filename="mainwindow.cpp" line="151"/>
         <source>Conversion successfully performed.</source>
         <translation>Konvertierung erfolgreich.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="169"/>
+        <location filename="mainwindow.cpp" line="160"/>
         <source>can&apos;t write to file
 
 </source>
@@ -141,40 +159,53 @@ Vielleicht ist der Typ der Quelldatei falsch:
 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="187"/>
+        <location filename="mainwindow.cpp" line="178"/>
         <source>Error - argument list</source>
         <translation>Fehler - Parameterliste</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="188"/>
+        <location filename="mainwindow.cpp" line="179"/>
         <source>Unexpected number of arguments</source>
         <translation>unerwartete Parameterzahl</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="197"/>
+        <location filename="mainwindow.cpp" line="188"/>
         <source>Set Source File</source>
         <translation>Quelldatei auswählen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="220"/>
+        <location filename="mainwindow.cpp" line="211"/>
         <source>Set Target File</source>
         <translation>Zieldatei auswählen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="260"/>
+        <location filename="mainwindow.cpp" line="251"/>
         <source>Select Input Definition File</source>
         <translation>Eingabedefinitions-Datei wählen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="262"/>
-        <location filename="mainwindow.cpp" line="270"/>
+        <location filename="mainwindow.cpp" line="253"/>
+        <location filename="mainwindow.cpp" line="261"/>
         <source>XML files (*.xml);;any file (*.*)</source>
         <translation>XML Dateien (*.xml);; Alle Dateien (*.*)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="268"/>
+        <location filename="mainwindow.cpp" line="259"/>
         <source>Select Output Definition File</source>
-        <translation>Eingabedefinitions-Datei auswählen</translation>
+        <translation>&amp;Ausgabedefinition wählen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="266"/>
+        <source>About htmLaTeX</source>
+        <translation>Über htmLaTeX</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="267"/>
+        <source>htmLaTeX 0.1
+A converter for JavaDoc-generated HTML to LaTeX(c) 2011 Björn (Kaiser|Baß)</source>
+        <translation>htmLaTeX 0.1
+Ein Konverter für JavaDoc generiertes HTML zu LaTeX
+(c) 2011 Björn (Kaiser|Baß)</translation>
     </message>
 </context>
 </TS>
--- htmlatex_en.ts
+++ htmlatex_en.ts
@@ -5,27 +5,27 @@
 <context>
     <name>CConsole</name>
     <message>
-        <location filename="cconsole.cpp" line="42"/>
+        <location filename="cconsole.cpp" line="41"/>
         <source>Read source file(s) --&gt; success</source>
         <translation>Read source file(s) --&gt; success</translation>
     </message>
     <message>
-        <location filename="cconsole.cpp" line="47"/>
+        <location filename="cconsole.cpp" line="46"/>
         <source>I/O error - the source file doesn&apos;t exit: </source>
         <translation>I/O error - the source file doesn&apos;t exit: </translation>
     </message>
     <message>
-        <location filename="cconsole.cpp" line="65"/>
+        <location filename="cconsole.cpp" line="64"/>
         <source>Perform conversion  --&gt; success</source>
         <translation>Perform conversion  --&gt; success</translation>
     </message>
     <message>
-        <location filename="cconsole.cpp" line="69"/>
+        <location filename="cconsole.cpp" line="68"/>
         <source>I/O error - can&apos;t write to file: </source>
         <translation>I/O error - can&apos;t write to file: </translation>
     </message>
     <message>
-        <location filename="cconsole.cpp" line="75"/>
+        <location filename="cconsole.cpp" line="74"/>
         <source>Error - unexpected number of arguments.</source>
         <translation>Error - unexpected number of arguments.</translation>
     </message>
@@ -34,64 +34,82 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="ui_mainwindow.h" line="93"/>
+        <location filename="ui_mainwindow.h" line="128"/>
         <source>HTMLatex</source>
         <translation>HTMLatex</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="42"/>
-        <location filename="ui_mainwindow.h" line="100"/>
+        <location filename="ui_mainwindow.h" line="136"/>
         <source>&amp;File</source>
         <translation>&amp;File</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="57"/>
-        <location filename="mainwindow.cpp" line="15"/>
-        <location filename="ui_mainwindow.h" line="94"/>
+        <location filename="mainwindow.ui" line="54"/>
+        <location filename="ui_mainwindow.h" line="137"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="64"/>
+        <location filename="ui_mainwindow.h" line="138"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="83"/>
+        <location filename="mainwindow.cpp" line="13"/>
+        <location filename="ui_mainwindow.h" line="129"/>
         <source>&amp;Quit</source>
         <translation>&amp;Quit</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="62"/>
-        <location filename="mainwindow.cpp" line="14"/>
-        <location filename="ui_mainwindow.h" line="95"/>
+        <location filename="mainwindow.ui" line="92"/>
+        <location filename="mainwindow.cpp" line="12"/>
+        <location filename="ui_mainwindow.h" line="130"/>
         <source>&amp;Convert</source>
         <translation>&amp;Convert</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="67"/>
-        <location filename="mainwindow.cpp" line="13"/>
-        <location filename="ui_mainwindow.h" line="96"/>
+        <location filename="mainwindow.ui" line="101"/>
+        <location filename="mainwindow.cpp" line="11"/>
+        <location filename="ui_mainwindow.h" line="131"/>
         <source>&amp;Open</source>
         <translation>&amp;Open</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="72"/>
-        <location filename="ui_mainwindow.h" line="97"/>
+        <location filename="mainwindow.ui" line="106"/>
+        <location filename="ui_mainwindow.h" line="132"/>
         <source>Set &amp;Replacement Table</source>
         <translation>Set &amp;Replacement Table</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="77"/>
-        <location filename="mainwindow.cpp" line="11"/>
-        <location filename="ui_mainwindow.h" line="98"/>
+        <location filename="mainwindow.ui" line="115"/>
+        <location filename="mainwindow.cpp" line="9"/>
+        <location filename="ui_mainwindow.h" line="133"/>
         <source>Set &amp;Input Definition</source>
         <translation>Set &amp;Input Definition</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="82"/>
-        <location filename="mainwindow.cpp" line="12"/>
-        <location filename="ui_mainwindow.h" line="99"/>
+        <location filename="mainwindow.ui" line="124"/>
+        <location filename="mainwindow.cpp" line="10"/>
+        <location filename="ui_mainwindow.h" line="134"/>
         <source>Set O&amp;utput Definition</source>
         <translation>Set O&amp;utput Definition</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="106"/>
+        <location filename="mainwindow.ui" line="133"/>
+        <location filename="ui_mainwindow.h" line="135"/>
+        <source>&amp;Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="97"/>
         <source>Error Reading Document</source>
         <translation>Error Reading Document</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="107"/>
+        <location filename="mainwindow.cpp" line="98"/>
         <source>The reading resulted in an empty document.
 Maybe an error occurred because of a wrong source file type:
 
@@ -102,13 +120,13 @@ Maybe an error occurred because of a wrong source file type:
 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="124"/>
-        <location filename="mainwindow.cpp" line="168"/>
+        <location filename="mainwindow.cpp" line="115"/>
+        <location filename="mainwindow.cpp" line="159"/>
         <source>I/O Error</source>
         <translation>I/O Error</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="125"/>
+        <location filename="mainwindow.cpp" line="116"/>
         <source>File doesn&apos;t exit:
 
 </source>
@@ -117,22 +135,22 @@ Maybe an error occurred because of a wrong source file type:
 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="156"/>
+        <location filename="mainwindow.cpp" line="147"/>
         <source>conversion successfully performed</source>
         <translation>conversion successfully performed</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="159"/>
+        <location filename="mainwindow.cpp" line="150"/>
         <source>Information</source>
         <translation>Information</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="160"/>
+        <location filename="mainwindow.cpp" line="151"/>
         <source>Conversion successfully performed.</source>
         <translation>Conversion successfully performed.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="169"/>
+        <location filename="mainwindow.cpp" line="160"/>
         <source>can&apos;t write to file
 
 </source>
@@ -141,40 +159,51 @@ Maybe an error occurred because of a wrong source file type:
 </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="187"/>
+        <location filename="mainwindow.cpp" line="178"/>
         <source>Error - argument list</source>
         <translation>Error - argument list</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="188"/>
+        <location filename="mainwindow.cpp" line="179"/>
         <source>Unexpected number of arguments</source>
         <translation>Unexpected number of arguments</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="197"/>
+        <location filename="mainwindow.cpp" line="188"/>
         <source>Set Source File</source>
         <translation>Set Source File</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="220"/>
+        <location filename="mainwindow.cpp" line="211"/>
         <source>Set Target File</source>
         <translation>Set Target File</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="260"/>
+        <location filename="mainwindow.cpp" line="251"/>
         <source>Select Input Definition File</source>
         <translation>Select Input Definition File</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="262"/>
-        <location filename="mainwindow.cpp" line="270"/>
+        <location filename="mainwindow.cpp" line="253"/>
+        <location filename="mainwindow.cpp" line="261"/>
         <source>XML files (*.xml);;any file (*.*)</source>
         <translation>XML files (*.xml);;any file (*.*)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="268"/>
+        <location filename="mainwindow.cpp" line="259"/>
         <source>Select Output Definition File</source>
         <translation>Select Output Definition File</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="266"/>
+        <source>About htmLaTeX</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="267"/>
+        <source>htmLaTeX 0.1
+A converter for JavaDoc-generated HTML to LaTeX(c) 2011 Björn (Kaiser|Baß)</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
